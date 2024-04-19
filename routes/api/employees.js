a5539bded9450ede97b9dccfe33704fb9f03ef1a
Shenandoah Bennett <github.com@shenanb.com>
2024-04-19 16:36:05 -0700

    Copied items from example, made some changes, doesn't run yet.

---
+++ routes/api/employees.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const statesController = require('../../controllers/statesController');
+
+router.route('/')
+    .get(statesController.getAllStates)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
+
+router.route('/:state')
+    .get(statesController.getState);
+
+module.exports = router;
