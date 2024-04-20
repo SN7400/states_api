6a03d56d266acb77e1975d3d756cf582cde2e31a
Shenandoah Bennett <github.com@shenanb.com>
2024-04-19 17:02:17 -0700

    More changes, runs and connects to DB now.

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -1,18 +1,18 @@
-const Employee = require('../model/Employee');
+const State = require('../model/State');
 
-const getAllEmployees = async (req, res) => {
-    const employees = await Employee.find();
-    if (!employees) return res.status(204).json({ 'message': 'No employees found.' });
-    res.json(employees);
+const getAllStates = async (req, res) => {
+    const states = await State.find();
+    if (!states) return res.status(204).json({ 'message': 'No states found.' });
+    res.json(states);
 }
 
-const createNewEmployee = async (req, res) => {
+const createNewState = async (req, res) => {
     if (!req?.body?.firstname || !req?.body?.lastname) {
         return res.status(400).json({ 'message': 'First and last names are required'})
     }
 
     try {
-        const result = await Employee.create({
+        const result = await State.create({
             firstname: req.body.firstname,
             lastname: req.body.lastname
         });
@@ -23,44 +23,44 @@ const createNewEmployee = async (req, res) => {
     }
 }
 
-const updateEmployee = async (req, res) => {
+const updateState = async (req, res) => {
     if (!req?.body?.id) {
         return res.status(400).json({ 'message': 'ID paramater is required'});
     }
 
-    const employee = await Employee.findOne({ _id: req.body.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
+    const state = await State.findOne({ _id: req.body.id }).exec();
+    if (!state) {
+        return res.status(204).json({ "message": `No state matches ID ${req.body.id}.` });
     }
-    if (req.body?.firstname) employee.firstname = req.body.firstname;
-    if (req.body?.lastname) employee.lastname = req.body.lastname;
-    const result = await employee.save();
+    if (req.body?.firstname) state.firstname = req.body.firstname;
+    if (req.body?.lastname) state.lastname = req.body.lastname;
+    const result = await state.save();
     res.json(result);
 }
 
-const deleteEmployee = async (req, res) => {
-    if (!req?.body?.id) return res.status(400).json({'message': 'Employee ID required.'})
-    const employee = await Employee.findOne({ _id: req.body.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
+const deleteState = async (req, res) => {
+    if (!req?.body?.id) return res.status(400).json({'message': 'State ID required.'})
+    const state = await State.findOne({ _id: req.body.id }).exec();
+    if (!state) {
+        return res.status(204).json({ "message": `No state matches ID ${req.body.id}.` });
     }
-    const result = await employee.deleteOne({ _id: req.body.id });
+    const result = await state.deleteOne({ _id: req.body.id });
     res.json(result);
 }
 
-const getEmployee = async (req, res) => {
-    if (!req?.params?.id) return res.status(400).json({'message': 'Employee ID required.'})
-    const employee = await Employee.findOne({ _id: req.params.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.params.id}.` });
+const getState = async (req, res) => {
+    if (!req?.params?.id) return res.status(400).json({'message': 'State ID required.'})
+    const state = await State.findOne({ _id: req.params.id }).exec();
+    if (!state) {
+        return res.status(204).json({ "message": `No state matches ID ${req.params.id}.` });
     }
-    res.json(employee);
+    res.json(state);
 }
 
 module.exports = {
-    getAllEmployees,
-    createNewEmployee,
-    updateEmployee,
-    deleteEmployee,
-    getEmployee
+    getAllStates,
+    createNewState,
+    updateState,
+    deleteState,
+    getState
 }
--- routes/api/states.js
+++ routes/api/states.js
@@ -4,9 +4,9 @@ const statesController = require('../../controllers/statesController');
 
 router.route('/')
     .get(statesController.getAllStates)
-    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployee)
-    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
-    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
+    //.post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployee)
+    //.put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
+    //.delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
 
 router.route('/:state')
     .get(statesController.getState);
